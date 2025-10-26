8d3da984c989ab81afabe51d010a180428cf97f1
Luana Aguilo <aguilo.luana@gmail.com>
2025-10-26 23:33:54 +0100

    Solved lab

--- index.js
+++ index.js
@@ -6,53 +6,58 @@ const s2 = "fed";
 const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
-
 // Concatenate the string variables into one new string
-
-
+const tongueTwister =
+  s1 +
+  " " +
+  s2 +
+  " " +
+  s3 +
+  " " +
+  s4 +
+  " " +
+  s5 +
+  " " +
+  s3 +
+  " " +
+  s2 +
+  " " +
+  s1 +
+  " " +
+  s4;
 // Print out the concatenated string
-
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
 const part1 = "java";
 const part2 = "script";
-
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+// Convert the last letter of part1 and part2 to uppercase and concatenate the string
+const JS =
+  part1.slice(0, part1.length - 1) +
+  part1[part1.length - 1].toUpperCase() +
+  part2.slice(0, part2.length - 1) +
+  part2[part2.length - 1].toUpperCase();
 // Print the cameLtaiL-formatted string
-
-
-
+console.log(JS);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
-
 // Calculate the tip (15% of the bill total)
-
-
+const tipAmount = billTotal * 0.15;
 // Print out the tipAmount
-
-
-
+console.log("$" + tipAmount);
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
-
 // Generate a random integer between 1 and 10 (inclusive)
-
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 // Print the generated random number
-
-
-
+console.log(randomNumber);
 /*******************************************
     Iteration 3.1 | Booleans
 *******************************************/
@@ -74,3 +79,10 @@ const expression5 = !a || !b;
 const expression6 = !(a || b);
 
 const expression7 = a && a;
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
